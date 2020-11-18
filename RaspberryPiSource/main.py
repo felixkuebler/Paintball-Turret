9ab8f61156449f5e8ab7d8ca013a6e6ba1f94281
Felix Kübler <felix.kuebler@variex.de>
2020-11-18 19:11:20 +0100

    print output of all args

--- RaspberryPiSource/main.py
+++ RaspberryPiSource/main.py
@@ -85,7 +85,7 @@ def option():
 
 @app.route('/feedback', methods=['GET'])
 def feedback():
-    print('Request Data: %s' % request.args.get('position'), file=sys.stdout, flush=True)
+    print('Request Data: %s' % request.args, file=sys.stdout, flush=True)
     #data = json.loads(request.args)
 
     if request.args.get('position'):
