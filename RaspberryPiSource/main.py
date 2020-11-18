6d6b14eed2a63df2b26a69308606aa08d5778926
Felix Kübler <felix.kuebler@variex.de>
2020-11-18 19:14:35 +0100

    retreve data from get request

--- RaspberryPiSource/main.py
+++ RaspberryPiSource/main.py
@@ -85,7 +85,7 @@ def option():
 
 @app.route('/feedback', methods=['GET'])
 def feedback():
-    print('Request Data: %s' % request.args, file=sys.stdout, flush=True)
+    print('Request Data: %s' % request.args.getlist, file=sys.stdout, flush=True)
     #data = json.loads(request.args)
 
     if request.args.get('position'):
