bd6b55b08a7d7bc1dcb05b3cee6dc282b2b1e4f0
felixkuebler <felix.kuebler@variex.de>
2024-07-28 16:28:17 +0200

    adapt userInput test

--- Software/RaspberryPiSource/TurretApp/turret/tests/test_userInput.py
+++ Software/RaspberryPiSource/TurretApp/turret/tests/test_userInput.py
@@ -1,43 +1,26 @@
 import time
+import sys
+
+sys.path.append('..')
+
 from arduinoSerialDevice import ArduinoSerialDevice
 
-print('Enter Arduino ID:\n- 0 for Bottom\n- 1 for Top')
+arduino = ArduinoSerialDevice(port='/dev/tty.usbserial-14220', baudrate=9600)
 
-arduinoSelect = input() == '0'
-arduino = 0
+print('Enter Arduino ID:\n- 0 for Yaw\n- 1 for Pitch')
+motorSelect = input()
 
-if arduinoSelect:
-	arduino = ArduinoSerialDevice(port='/dev/ttyUSB0', baudrate=9600)
-	time.sleep(2)
+print('Enter Motion Mode:\n- 0 for Angle Absolute \n - 1 for Angle Relative \n- 2 for Speed')
+modeSelect = input()
 
-	arduino.motorAttach(5,6)
-	arduino.motorSetGearRatio(10) # for bottom
-	arduino.motorInvertDir(1) # for bottom
-	arduino.motorSetRange(18000) # for bottom 180°
-	arduino.motorSetMinSpeed(75)
-	arduino.motorSetMaxSpeed(125)
+if modeSelect == '0':
+	print('Enter absolute Angle:\n(terminate with \"q\")\n-----------------------------')
 
-else:
-	arduino = ArduinoSerialDevice(port='/dev/ttyUSB1', baudrate=9600)
-	time.sleep(2)
-
-	arduino.motorAttach(5,6)
-	arduino.motorSetGearRatio(5) # for top
-	arduino.motorInvertDir(0) # for top
-	arduino.motorSetRange(9000) # for top 90°
-	arduino.motorSetMinSpeed(100)
-	arduino.motorSetMaxSpeed(200)
-
-
-print('Enter Motion Mode:\n- 0 for Angle\n- 1 for Speed')
-modeSelect = input() == '0'
-
-if modeSelect:
-	print('Enter Angle:\n(terminate with \"q\")\n-----------------------------')
+elif modeSelect == '1':
+	print('Enter relative Angle:\n(terminate with \"q\")\n-----------------------------')
 
 else:
 	print('Enter Speed:\n(terminate with \"q\")\n-----------------------------')
-
 
 while True:
 
@@ -46,7 +29,19 @@ while True:
 	if value == 'q':
 		break
 
-	if modeSelect:
-		arduino.motorWritePosition(int(value))
+	if motorSelect == '0':
+
+		if modeSelect == '0':
+			arduino.motorYawWritePositionAbsolute(int(value))
+		elif modeSelect == '1':
+			arduino.motorYawWritePositionRelative(int(value))
+		else:
+			arduino.motorYawWriteSpeed(int(value))
+
 	else:
-		arduino.motorWriteSpeed(int(value))
+		if modeSelect == '0':
+			arduino.motorPitchWritePositionAbsolute(int(value))
+		elif modeSelect == '1':
+			arduino.motorPitchWritePositionRelative(int(value))
+		else:
+			arduino.motorPitchWriteSpeed(int(value))
