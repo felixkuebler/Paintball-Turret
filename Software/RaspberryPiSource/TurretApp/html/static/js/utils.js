216fd08170d70172d8090337ea051ca9c53cf8b9
Felix Kübler <felix.kuebler@variex.de>
2024-08-05 10:50:47 -0500

    fix of the mapping function

--- Software/RaspberryPiSource/TurretApp/html/static/js/utils.js
+++ Software/RaspberryPiSource/TurretApp/html/static/js/utils.js
@@ -84,6 +84,6 @@ function httpPostAsync(url, data){
 
 
 function map(x, in_min, in_max, out_min, out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  return ((x - in_min) * ((out_max - out_min) / (in_max - in_min))) + out_min;
 }
 
